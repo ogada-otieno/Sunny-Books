24d8bb79fd198f1161e29fd6e9fbc5aca747fb29
PapitoSpence <PapitoSpence>
2023-03-29 15:32:06 +0300

    Cart done

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import SignUp from "./SignUp";
 import Login from "./Login";
 import UserProfile from "./UserProfile";
+import Cart from "./Cart";
 function App() {
   return (
     <div className="App">
     <SignUp />
     <Login />
     <UserProfile />
+    <Cart />
     </div>
   );
 }
