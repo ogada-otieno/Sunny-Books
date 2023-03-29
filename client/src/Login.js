aefa5222d3f0e7d7fb39b7752c25ca17d47a12df
PapitoSpence <PapitoSpence>
2023-03-29 12:44:24 +0300

    signup and Login

---
+++ client/src/Login.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleResetPasswordClick = () => {
+    // handle reset password logic here
+  };
+
+  const handleSignUpClick = () => {
+    // handle sign-up redirect logic here
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle login logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">Log In</button>
+      <a href="#" onClick={handleResetPasswordClick}>
+        Reset Password
+      </a>
+      <a href="#" onClick={handleSignUpClick}>
+        Sign Up
+      </a>
+    </form>
+  );
+};
+
+export default Login;
---
+++ client/src/SignUp.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Submit form data to backend or perform validation
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
+};
+
+export default SignUpForm;
