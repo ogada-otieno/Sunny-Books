9ac77e51ae14dc8d6cab3a57e483ea3709102601
PapitoSpence <PapitoSpence>
2023-03-31 19:17:12 +0300

    updated Nabar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import {Link, Outlet} from 'react-router-dom'
+import "./navbar.css"
+import { Link, Outlet } from 'react-router-dom'
 
 const Navbar = () => {
     return (
         <>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/signUp">signUp</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/Profile">Profile</Link>
-                    </li>
-                    <li>
-                        <Link to="/Cart">Cart</Link>
-                    </li>
-                </ul>
+                <h1>Logo</h1>
+                <div className='navbar'>
+                    <Link to="/signUp">signUp</Link>
+
+                    <Link to="/login">Login</Link>
+
+                    <Link to="/Purchase Book">Purchase a Book</Link>
+
+                    <Link to="/Cart">Cart</Link>
+                </div>
+
             </nav>
 
             <Outlet />
