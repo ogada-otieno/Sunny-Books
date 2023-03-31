570c79e72d997cbe677f9a690e9ce17ccafea942
RoyZiO <nziokinroy@gmail.com>
2023-03-31 11:31:23 +0300

    Created BookList

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
-import BookCard from './components/BookCard'
+//import BookCard from './components/BookCard'
+import BookList from './components/BookList';
 
 function App() {
   return (
     <div className="App">
-      <BookCard/>
+      <BookList/>
     </div>
   );
 }
