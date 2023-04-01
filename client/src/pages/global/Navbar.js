291e61d234126945481df1667e7deee972593ff4
Rodgers Ogada <ogadarodgers@gmail.com>
2023-04-01 17:05:07 +0300

    set state of cart and added badge to cart icon to indicate quantity count inside cart.

--- client/src/pages/global/Navbar.js
+++ client/src/pages/global/Navbar.js
@@ -9,10 +9,12 @@ import {
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { useNavigate } from "react-router-dom";
 import { shades } from "../../theme";
+import { setIsCartOpen } from "../../state";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  //   const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart.cart);
   return (
     <Box
       display="flex"
@@ -52,9 +54,26 @@ const Navbar = () => {
           <IconButton sx={{ color: "black" }}>
             <PersonOutline />
           </IconButton>
-          <IconButton sx={{ color: "black" }}>
-            <AddShoppingCartIcon />
-          </IconButton>
+          <Badge
+            badgeContent={cart.length}
+            color="secondary"
+            invisible={cart.length === 0}
+            sx={{
+              "& .Mui-Badge-badge": {
+                right: 5,
+                top: 5,
+                padding: "0, 4px",
+                height: "14px",
+              },
+            }}
+          >
+            <IconButton
+              onClick={() => dispatch(setIsCartOpen({}))}
+              sx={{ color: "black" }}
+            >
+              <AddShoppingCartIcon />
+            </IconButton>
+          </Badge>
           <IconButton sx={{ color: "black" }}>
             <MenuOutlined />
           </IconButton>
