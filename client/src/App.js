6f185f0591d97fbeffbb2b4b8ab558803b5f7b66
PapitoSpence <PapitoSpence>
2023-04-01 11:23:25 +0300

    added new component

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
-import './style.css';
-import SignUp from "./SignUp";
-import { Routes, Route } from 'react-router-dom'
-import Login from "./Login";
-import Navbar from "./Navbar";
-import UserProfile from "./UserProfile";
-import Cart from "./Cart";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Book from "./components/Book";
+import Cart from "./components/Cart";
+import Register from "./components/Register";
+import About from "./components/About";
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
+    <div>
+      <NavBar />
       <Routes>
-        <Route path="/SignUp" element={<SignUp />}></Route>
-        <Route path="/Login" element={<Login />}></Route>
-        <Route path="/Purchase Book" element={<UserProfile />}></Route>
-        <Route path="/Cart" element={<Cart />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Book />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
   );
