5e5536842a81543ec2a8a5c3e56cf5d95796ca9b
PapitoSpence <PapitoSpence>
2023-04-01 11:25:40 +0300

    updated home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css";
+
+function Home() {
+  return (
+    <div className="home-content">
+      <h2 className="home-title">Welcome to Our Book Store</h2>
+      <p className="home-text">
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur,{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Home;
