a6f58b4b88b2e53561437b32a308c22e46b533b4
Rodgers Ogada <ogadarodgers@gmail.com>
2023-04-02 23:02:45 +0300

    Code to persist the cart even when the page is refresh

--- client/src/index.js
+++ client/src/index.js
@@ -6,19 +6,26 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./theme";
-import { Provider, } from "react-redux";
-import store from './store'
+import { Provider } from "react-redux";
+import store from "./store";
+
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+let persistor = persistStore(store);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import cartReducer from "./state";
+
+// documentation for persisting state in redux: https://dev.to/dawnind/persist-redux-state-with-redux-persist-3k0d
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const reducers = combineReducers({ cart: cartReducer });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 
 const store = configureStore({
-  reducer: { cart: cartReducer },
+  // reducer: { cart: cartReducer },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+
+
+
 
 export default store;
